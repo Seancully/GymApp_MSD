29aa1aff745517c8b00e062347f0d415750e8144
Sean <Sean@MacBook-Air.local>
2023-11-15 16:25:57 +0000

    3 new activities + links from main -> each

--- app/src/main/java/com/example/gymapp_msd/MainActivity.java
+++ app/src/main/java/com/example/gymapp_msd/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.gymapp_msd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize buttons
+        Button MotivationButton = findViewById(R.id.MotivationButton);
+        Button WorkoutButton = findViewById(R.id.WorkoutButton);
+        Button CalorieButton = findViewById(R.id.CalorieButton);
+
+        // set click listeners
+        MotivationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MotivationActivity.class));
+            }
+        });
+
+        WorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WorkoutActivity.class));
+            }
+        });
+
+        CalorieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CalorieActivity.class));
+            }
+        });
     }
 }
