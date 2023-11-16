0d5c03a0d8e7699888cf4c98d1cf41af8d619558
Sean <Sean@MacBook-Air.local>
2023-11-16 10:02:47 +0000

    activity_main.xml new background

--- app/src/main/java/com/example/gymapp_msd/MotivationActivity.java
+++ app/src/main/java/com/example/gymapp_msd/MotivationActivity.java
@@ -3,6 +3,7 @@ package com.example.gymapp_msd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MotivationActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MotivationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motivation);
+
+        // for back button
+        if (getSupportActionBar()!= null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle back button action
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
