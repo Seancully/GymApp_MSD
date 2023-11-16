5c594d17b10504ae8c7857cc0627226b48ac1b42
Sean <Sean@MacBook-Air.local>
2023-11-16 16:50:13 +0000

    Step counter done but no simulation (yet)

--- app/src/main/java/com/example/gymapp_msd/WorkoutActivity.java
+++ app/src/main/java/com/example/gymapp_msd/WorkoutActivity.java
@@ -2,6 +2,7 @@ package com.example.gymapp_msd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -9,13 +10,25 @@ import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class WorkoutActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor stepCounterSensor;
+    private int stepCount = 0;
+    private TextView tvStepCount;
+    private int initialStepCount = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        tvStepCount = findViewById(R.id.tvStepCount);
 
         // Setting up the title with SpannableString for color formatting
         TextView title = findViewById(R.id.titleHealthHarbor);
@@ -25,6 +38,48 @@ public class WorkoutActivity extends AppCompatActivity {
         spannableString.setSpan(new ForegroundColorSpan(Color.WHITE), 6, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE); // "Harbor" in black
         title.setText(spannableString);
 
+        // Initialize the Sensor Manager and the Step Counter Sensor
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if (stepCounterSensor != null) {
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            // Handle the absence of a step counter sensor
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            if (initialStepCount < 0) {
+                // Initialize with the first value to effectively reset the step count
+                initialStepCount = (int) event.values[0];
+            }
+
+            // Calculate steps taken since the activity started
+            stepCount = (int) event.values[0] - initialStepCount;
+            tvStepCount.setText("Steps: " + stepCount);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (stepCounterSensor != null) {
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_UI);
+        }
     }
 
     @Override
