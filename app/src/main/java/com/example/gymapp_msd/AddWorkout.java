123b6a70b397d2314cc001d06932a1e60089a202
Sean <Sean@MacBook-Air.local>
2023-11-28 14:49:58 +0000

    small change for one callback/gesture

--- app/src/main/java/com/example/gymapp_msd/AddWorkout.java
+++ app/src/main/java/com/example/gymapp_msd/AddWorkout.java
@@ -58,7 +58,7 @@ public class AddWorkout extends AppCompatActivity {
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AddWorkout.this, MainActivity.class);
+                Intent intent = new Intent(AddWorkout.this, LiftActivity.class);
                 startActivity(intent);
                 finish();
             }
